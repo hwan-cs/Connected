35565a3863038a50addc21f0a2809107d9075e2c
hwan-cs <hwann1005@gmail.com>
2022-09-09 19:00:06 +0900

    Test

--- Connected/ViewController.swift
+++ Connected/ViewController.swift
@@ -9,14 +9,11 @@ import UIKit
 
 class ViewController: UIViewController
 {
-
     override func viewDidLoad()
     {
         super.viewDidLoad()
         print("test")
         // Do any additional setup after loading the view.
     }
-
-
 }
 
