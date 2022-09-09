9f766fe4acd96c9de9ec6915199ab1a77eeb960a
hwan-cs <hwann1005@gmail.com>
2022-09-09 18:36:38 +0900

    Initial Commit

---
+++ Connected/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Connected
+//
+//  Created by Jung Hwan Park on 2022/09/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
