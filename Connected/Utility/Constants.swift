7b76a9e3c48853fd0273886175c9524f3b391c6a
Jung Hwan Park <hwann1005@gmail.com>
2022-10-04 03:45:12 +0900

    gitignore

---
+++ Connected/Utility/Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Constants.swift
+//  Connected
+//
+//  Created by Jung Hwan Park on 2022/09/18.
+//
+import Foundation
+import UIKit
+
+struct K
+{
+    static let mainColor = UIColor(red: 0.02, green: 0.78, blue: 0.51, alpha: 1.00)
+    static var didSignupNewUser = false
+    static var newUserEmail = ""
+    static var seconds = 60
+    static var isTimerRunning = false
+    static let myChatCellNibName = "ChatTableViewCell"
+    static let yourChatCellNibName = "RecChatTableViewCell"
+    static let myChatCellID = "MyChatCell"
+    static let yourChatCellID = "YourChatCell"
+    static var didInit = false
+    static let myTextCellNibName = "TextChatTableViewCell"
+    static let myTextCellID = "MyTextCell"
+    static let yourTextCellNibName = "RecTextChatTableViewCell"
+    static let yourTextCellID = "YourTextCell"
+    static let GoogleMapsAPIKey = "AIzaSyCa2-FW5UZaId_S6QmuNAAtOqTEEDKUcpM"
+}
