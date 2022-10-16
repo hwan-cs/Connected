636b06ff1e9fd9b40005a11f590e183dcf5f9244
Jung Hwan Park <hwann1005@gmail.com>
2022-10-17 03:16:03 +0900

    delete Pods from remote repo

--- Pods/Target Support Files/KFImageViewer/KFImageViewer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_KFImageViewer : NSObject
-@end
-@implementation PodsDummy_KFImageViewer
-@end
--- Pods/Target Support Files/VBRRollingPit/VBRRollingPit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_VBRRollingPit : NSObject
-@end
-@implementation PodsDummy_VBRRollingPit
-@end
